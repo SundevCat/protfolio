3f50bc169d74a95adb1ad9b39c8beba9d41af40b
Chirapong Phomphoo <Chirapong@PLANTOYS.NET>
2024-07-18 11:30:00 +0700

    respon profile

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import logo from '../../assets/profile.png'
 import { Col, Row } from 'react-bootstrap'
 import { ReactTyped } from 'react-typed'
@@ -8,7 +8,7 @@ function Profile(prop) {
         <div>
             <Row>
                 <Col ></Col>
-                <Col >
+                <Col xs={7} sm={6} md={4} >
                     <img className='w-100 shadow-lg rounded-circle' src={logo} />
                 </Col>
                 <Col ></Col>
